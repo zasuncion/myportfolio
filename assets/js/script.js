b7af29a7d01e4b120a8ba7847b3575fd310fbc52
Daniel Bogdoll <bogdoll@fzi.de>
2022-11-30 09:35:59 +0100

    Removed button

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+// sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
 // custom select variables
