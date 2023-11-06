b52dc1996c0c630dbbf6012e485a72198f39c769
Daniel Bogdoll <daniel.bogdoll@rwth-aachen.de>
2023-11-06 21:10:08 +0000

    reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
 
-    if (selectedValue === "all") {
+    if (selectedValue === "all publications") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
       filterItems[i].classList.add("active");
